5e12b059a223744765f43e1d05cbcdeae2e27fc8
makpande <mak_pande@yahoo.com>
2016-03-10 12:01:52 -0500

    checkpoint

--- scripts/album.js
+++ scripts/album.js
@@ -62,27 +62,6 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-//var child = document.getElementsByClassName('album-view-title')[0];
-//var noParent = document.querySelector('html'); 
-//
-//var findParentByClassName = function(element, targetClass) {
-//        var currentParent = element.parentElement;
-//    if (currentParent) {
-//        while (currentParent.className && currentParent.className != targetClass) {
-//            currentParent = currentParent.parentElement;
-//        }
-//        
-//        if (currentParent.className == targetClass) {
-//            return currentParent;
-//        } else {
-//            alert("No parent with that class name found.");
-//        }
-//    } else {
-//        alert("No parent found");    
-//    
-//    }
-//};
-
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
